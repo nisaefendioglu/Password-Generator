535b954f4394af5ba4537dcffb393d53d960426e
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-07-16 00:05:20 +0300

    add project design

--- app/src/main/java/com/nisaefendioglu/passwordgenerator/MainActivity.kt
+++ app/src/main/java/com/nisaefendioglu/passwordgenerator/MainActivity.kt
@@ -2,10 +2,13 @@ package com.nisaefendioglu.passwordgenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
