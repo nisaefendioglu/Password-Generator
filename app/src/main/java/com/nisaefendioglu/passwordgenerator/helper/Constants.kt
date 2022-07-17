1720abb2bc2d0c512ce0b40505fcc78d26330891
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-07-17 09:37:25 +0300

    add constants

---
+++ app/src/main/java/com/nisaefendioglu/passwordgenerator/helper/Constants.kt
@@ -0,0 +1,8 @@
+package com.nisaefendioglu.passwordgenerator.helper
+
+object Constants {
+
+    const val BASE_URL = "https://www.passwordrandom.com/"
+    const val END_POINT = "query?command=password&format=json&count=1"
+
+}
