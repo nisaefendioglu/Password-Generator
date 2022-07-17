1d7cfbba5e0d3430c774e1fb67d5555827a45f36
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-07-17 10:15:35 +0300

    fix Api Service

--- app/src/main/java/com/nisaefendioglu/passwordgenerator/api/ApiService.kt
+++ app/src/main/java/com/nisaefendioglu/passwordgenerator/api/ApiService.kt
@@ -1,13 +1,13 @@
 package com.nisaefendioglu.passwordgenerator.api
 
 import com.nisaefendioglu.passwordgenerator.helper.Constants
-import com.nisaefendioglu.passwordgenerator.model.PasswordModel
+import com.nisaefendioglu.passwordgenerator.model.PasswordResponse
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiService {
 
     @GET(Constants.END_POINT)
-    suspend fun getPasswordGenerator(): Response<PasswordModel>
+    suspend fun getPasswordGenerator(): Response<PasswordResponse>
 
 }
