938b9f06555e70386ab52fb1c6d4e76dfcf2f985
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-07-18 09:28:38 +0300

    fix

--- app/src/main/java/com/nisaefendioglu/passwordgenerator/MainActivity.kt
+++ app/src/main/java/com/nisaefendioglu/passwordgenerator/MainActivity.kt
@@ -2,11 +2,26 @@ package com.nisaefendioglu.passwordgenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.nisaefendioglu.passwordgenerator.databinding.ActivityMainBinding
+import com.nisaefendioglu.passwordgenerator.viewmodel.PasswordGeneratorViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel : PasswordGeneratorViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.generatePassword.apply {
+            setOnClickListener {
+             //   binding.password.text = viewModel.responsePassword.toString()
+            }
+        }
 
     }
+
 }
